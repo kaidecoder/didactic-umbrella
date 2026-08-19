32702fb226a34e46398870ccfdfbc0ef44327638
kaidecoder <131601358+kaidecoder@users.noreply.github.com>
2026-08-19 08:52:27 -0400

    Fix configuration asset base paths

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite"
-import react from "@vitejs/react-refresh" // or your standard plugin
+import react from "@vitejs/plugin-react"
 
 export default defineConfig({
-  base: "/prenticemath-frontend/", // Must match your repo name exactly
+  base: "/prenticemath-frontend/",
   plugins: [react()],
 })
